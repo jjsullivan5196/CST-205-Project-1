41048cb9b9a3c464b87ee49ea3ff5b51b5c0fb46
John Sullivan <jsullivan@csumb.edu>
2015-09-07 17:22:41 -0700

    Slow progress on difference filter

--- removeoutlier.py
+++ removeoutlier.py
@@ -7,9 +7,15 @@ for x in range(0,8):
   pictures.append(makePicture(picfolder + str(x + 1) + ".png"))
   picxels.append(getAllPixels(pictures[x]))
 
+pictureSafe = duplicatePicture(pictures[0])
+picxelsSafe = getAllPixels(pictureSafe)
+
 for x in range(1,8):
-  for pix1, pix2 in zip(picxels[0], picxels[x]):
-    if( getColor(pix1) == getColor(pix2) ):
+  for pix1, pix2, pixSafe in zip(picxels[0], picxels[x], picxelsSafe):
+    origColor = getColor(pixSafe)
+    compColor = getColor(pix2)
+    dist = distance(origColor, compColor)
+    if ( dist < 3):
       setColor(pix1, replaceColor)
 
 show(pictures[0])
