3b5173d2b8cb683c34ef3cbb669b6129e1f5f7d2
John Sullivan <jsullivan@csumb.edu>
2015-09-07 17:27:45 -0700

    Forgot comments, whoops.

--- removeoutlier.py
+++ removeoutlier.py
@@ -1,22 +1,30 @@
+#Create lists for images and individual pixels of those images
 pictures = list()
 picxels = list()
+
+#Ask user for folder and replacement color for similarities
 picfolder = pickAFolder()
 replaceColor = pickAColor()
 
+#Populate picture and pixel lists
 for x in range(0,8):
   pictures.append(makePicture(picfolder + str(x + 1) + ".png"))
   picxels.append(getAllPixels(pictures[x]))
 
+#Create a safe copy of the first image to compare against
 pictureSafe = duplicatePicture(pictures[0])
 picxelsSafe = getAllPixels(pictureSafe)
 
+#Iterate through images and compare
 for x in range(1,8):
+  #Three set iteration for first image, test image, and the safe copy
   for pix1, pix2, pixSafe in zip(picxels[0], picxels[x], picxelsSafe):
-    origColor = getColor(pixSafe)
-    compColor = getColor(pix2)
-    dist = distance(origColor, compColor)
+    origColor = getColor(pixSafe) #Color of pixel from safe image
+    compColor = getColor(pix2) #Color of pixel from test image
+    dist = distance(origColor, compColor) #Distance of color
     if ( dist < 3):
-      setColor(pix1, replaceColor)
+      setColor(pix1, replaceColor) #If the distance is less than 3 units, mark it as a similar pixel
 
+#Show and write results
 show(pictures[0])
 writePictureTo(pictures[0], pickAFile())
