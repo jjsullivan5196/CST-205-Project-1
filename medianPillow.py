1f2d8d6d9d1a15ed4e308c3df46fac45730016fc
jjsullivan5196 <jjsullivan5196@gmail.com>
2015-09-15 20:53:11 -0700

    Stuff

--- medianPillow.py
+++ medianPillow.py
@@ -6,35 +6,38 @@ from tkinter.filedialog import askdirectory,asksaveasfilename
 from tkinter.messagebox import showinfo,showwarning,showerror,askquestion
 from PIL import Image
 
-root = tk.Tk()
-root.withdraw()
-showinfo('Median Filter', 'Select a directory with visually similar images') #Ask user for image directory
-picDir = askdirectory()
-pixels = list()
-tempPic = 0
+def main():
+    root = tk.Tk()
+    root.withdraw()
+    showinfo('Median Filter', 'Select a directory with visually similar images') #Ask user for image directory
+    picDir = askdirectory()
+    pixels = list()
+    tempPic = 0
 
-for file in os.listdir(picDir): #Populate lists
-    if file.endswith((".png", ".PNG", ".jpg", ".JPG", ".jpeg", ".JPEG")):
-        tempPic = Image.open(picDir + os.sep + file)
-        pixels.append(tempPic.getdata())
-if(len(pixels) < 2): #Not enough images exception
-    showerror('Error', 'Not enough images. Please restart with the correct directory')
-    exit()
+    for file in os.listdir(picDir): #Populate lists
+        if file.endswith((".png", ".PNG", ".jpg", ".JPG", ".jpeg", ".JPEG")):
+            tempPic = Image.open(picDir + os.sep + file)
+            pixels.append(tempPic.getdata())
+    if(len(pixels) < 2): #Not enough images exception
+        showerror('Error', 'Not enough images. Please restart with the correct directory')
+        exit()
 
-newPicture = Image.new(tempPic.mode, tempPic.size) #Create new picture with matching dimensions
-newPixels = list(newPicture.getdata())
+    newPicture = Image.new(tempPic.mode, tempPic.size) #Create new picture with matching dimensions
+    newPixels = list(newPicture.getdata())
 
-for idx, pixel in enumerate(newPixels): #The medianfinal method, the Pillow way
-    r,g,b = list(),list(),list()
-    for pix in pixels:
-        r.append(pix[idx][0])
-        g.append(pix[idx][1])
-        b.append(pix[idx][2])
-    newPixels[idx] = (int(median(r)),int(median(g)),int(median(b)))
+    for idx, pixel in enumerate(newPixels): #The medianfinal method, the Pillow way
+        r,g,b = list(),list(),list()
+        for pix in pixels:
+            r.append(pix[idx][0])
+            g.append(pix[idx][1])
+            b.append(pix[idx][2])
+        newPixels[idx] = (int(median(r)),int(median(g)),int(median(b)))
 
-newPicture.putdata(newPixels)
-newPicture.show()
-askSave = askquestion('Save', 'Would you like to save the new image?', icon='info')
-if(askSave == 'yes'):
-    fileName = asksaveasfilename()
-    newPicture.save(fileName)
+    newPicture.putdata(newPixels)
+    newPicture.show()
+    askSave = askquestion('Save', 'Would you like to save the new image?', icon='info')
+    if(askSave == 'yes'):
+        fileName = asksaveasfilename()
+        newPicture.save(fileName)
+
+main()
