f702b1536e1bd1ee3d3a453bafe3d33a082f310c
jjsullivan5196 <jjsullivan5196@gmail.com>
2015-09-15 20:07:57 -0700

    Who wants pillows!
    I do!

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # CST-205-Project-1
 medianfinal.py is/will be the final result of this project. It's commented and everything for it to work is present in the source file. The rest is for arhive:
 
-* marksimilarities.py
-* flipx.py
-* flipxclean.py
+* marksimilarities.py -- Marks similar pixels with a selected color
+* flipx.py -- Translates pixels over the x axis
+* flipxclean.py -- Clean version of flipx.py
+* medianhex.py -- A less-correct hex-based version of medianfinal, gives surprising results
 
 Original images are located in images/originals use the folder to test the application.
+
+Also, for funsies:
+
+* medianPillow.py -- Same method as medianfinal.py, except it uses Python 3 and the Pillow PIL-fork instead of JES. Runs 75% faster!
+
+If I could have, I would have wanted to turn this one in as the final version, as it works much better than medianfinal.py. They both essentially do the same thing, but medianPillow seems to be faster and more correct.
+
+Results for the various programs are organized in images/results. Originals are in images/originals.
---
+++ medianPillow.py
@@ -0,0 +1,41 @@
+#medianPillow.py -- Same method as medianfinal.py but using Python 3 and the Pillow PIL-fork
+import os, sys
+from statistics import median
+import tkinter as tk
+from tkinter.filedialog import askdirectory,asksaveasfilename
+from tkinter.messagebox import showinfo,showwarning,showerror,askquestion
+from PIL import Image
+
+root = tk.Tk()
+root.withdraw()
+showinfo('Median Filter', 'Select a directory with visually similar images') #Ask user for image directory
+picDir = askdirectory()
+pictures = list()
+pixels = list()
+
+for file in os.listdir(picDir): #Populate lists
+    if file.endswith((".png", ".PNG", ".jpg", ".JPG", ".jpeg", ".JPEG")):
+        index = len(pictures)
+        pictures.append(Image.open(picDir + os.sep + file))
+        pixels.append(pictures[index].getdata())
+if(len(pictures) < 2): #Not enough images exception
+    showerror('Error', 'Not enough images. Please restart with the correct directory')
+    exit()
+
+newPicture = Image.new(pictures[0].mode, pictures[0].size) #Create new picture with matching dimensions
+newPixels = list(newPicture.getdata())
+
+for idx, pixel in enumerate(newPixels): #The medianfinal method, the Pillow way
+    r,g,b = list(),list(),list()
+    for pix in pixels:
+        r.append(pix[idx][0])
+        g.append(pix[idx][1])
+        b.append(pix[idx][2])
+    newPixels[idx] = (int(median(r)),int(median(g)),int(median(b)))
+
+newPicture.putdata(newPixels)
+newPicture.show()
+askSave = askquestion('Save', 'Would you like to save the new image?', icon='info')
+if(askSave == 'yes'):
+    fileName = asksaveasfilename()
+    newPicture.save(fileName)
